838cd5e9622050027e322d0cc824a40e0754e6c2
Ivan Sklyarov <sklyarov.161@gmail.com>
2014-04-28 14:34:29 +0400

    complete todo application

---
+++ public/js/controllers/todo_controller.js
@@ -0,0 +1,32 @@
+App.TodoController = Ember.ObjectController.extend({
+    actions: {
+        editTodo: function(){
+            this.set('isEditing',true);
+        },
+        acceptChanges: function(){
+            this.set('isEditing',false);
+            if(Ember.isEmpty(this.get('model.title'))){
+                this.send('removeTodo');
+            } else {
+                this.get('model').save();
+            }
+        },
+        removeTodo: function(){
+            var model = this.get('model');
+            model.deleteRecord();
+            model.save();
+        }
+    },
+    isEditing: false,
+    isCompleted: function(key,value) {
+        var model = this.get('model');
+
+        if (value === undefined) {
+            return model.get('isCompleted');
+        } else {
+            model.set('isCompleted',value);
+            model.save();
+            return value;
+        }
+    }.property('model.isCompleted')
+});
